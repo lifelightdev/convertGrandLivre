8fa099263966e941b6da48755ebffcac6f363a0a
Life Light <lifelightdev@gmail.com>
2022-07-24 13:09:51 +0200

    ajout de tests d'extraction

--- extract_file.py
+++ extract_file.py
@@ -2,7 +2,19 @@ import pdfplumber
 
 
 def extract_file(my_file: str):
+    les_pages = []
+    with pdfplumber.open(my_file) as pdf:
+        for i in range(0, len(pdf.pages)):
+            page = pdf.pages[i].extract_table()
+            les_pages = les_pages + page
+    return page
 
-    pdf = pdfplumber.open(my_file)
-    page = pdf.pages[0]
-    return page.extract_table()
+def extract_page_in_file(my_file: str, num_page: int):
+    with pdfplumber.open(my_file) as pdf:
+        page = pdf.pages[num_page].extract_table()
+    return page
+
+def extract_nombre_de_page(my_file: str):
+    with pdfplumber.open(my_file) as pdf:
+        nombre = len(pdf.pages)
+    return nombre
--- extract_file_test.py
+++ extract_file_test.py
@@ -1,12 +1,19 @@
 import unittest
-
-from extract_file import extract_file
+from extract_file import extract_file, extract_page_in_file, extract_nombre_de_page
 
 
 class TestExtractFile(unittest.TestCase):
     def test_extract_file(self):
         file = extract_file('Grand_livre de test.pdf')
         self.assertTrue(file)
+
+    def test_extract_file_page_1(self):
+        file = extract_page_in_file('Grand_livre de test.pdf', 1)
+        self.assertTrue(file)
+
+    def test_extract_nombre_de_page(self):
+        nombre = extract_nombre_de_page('Grand_livre de test.pdf')
+        self.assertEqual(nombre, 2)
 
 if __name__ == '__main__':
     unittest.main()
